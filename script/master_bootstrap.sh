2212c027f452b42259b9a100f7677ef06fd04951
0xmks <0xmks@users.noreply.github.com>
2017-03-04 09:28:48 +0900

    Update master_bootstrap.sh

--- script/master_bootstrap.sh
+++ script/master_bootstrap.sh
@@ -2,9 +2,11 @@
 
 # locale
 sudo localectl set-locale LANG=ja_JP.utf8
+sudo localectl status
 
 # timezone
 sudo timedatectl set-timezone Asia/Tokyo
+sudo timedatectl status
 
 # git
 sudo yum install -y git
